47bfa77e003d6cdbf15107275097936296e89774
narendar kumar <nkumar010896@gmail.com>
2018-09-16 21:06:42 +0530

    edited

--- Transform/Transform.cs
+++ Transform/Transform.cs
@@ -2,14 +2,17 @@
 using System.Linq;
 using System.Collections.Generic;
 public delegate int Smp(int x);
+//Delegate is used to perform the lambda expression
 namespace Transform
 {
     public static class Transform 
     {
+        //delegate is a pointer and object of delegate will point to the function
         public static int[] Map(this int[] a, Smp value )
         {
             for(int i=0;i<a.Length;i++)
             {
+                //value is a object and it will call  a function that is present in the Map and perform the operation
                 a[i]=value(a[i]);
             }
             return a;
